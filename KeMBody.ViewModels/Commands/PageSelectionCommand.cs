d56b0a014bea57ca76e04127c9c3d0d48b51cf10
Adam Owczarzak <adam.owczarzak.job@gmail.com>
2022-04-24 21:31:54 +0200

    Expanding NumericUpDown control.
    Expanding NumericUpDown control.

--- KeMBody.ViewModels/Commands/PageSelectionCommand.cs
+++ KeMBody.ViewModels/Commands/PageSelectionCommand.cs
@@ -15,7 +15,7 @@ namespace KeMBody.ViewModels
         public override void Execute(object applicationPage)
         {
             mainWindowViewModel.ApplicationPage = applicationPage.ConvertTo<ApplicationPage>();
-            mainWindowViewModel.SelectedPage = new AddMeasurementsSummaryViewModel();
+            mainWindowViewModel.SelectedPage = new WeightViewModel();
         }
     }
 }
--- KeMBody/CustomControls/NumericUpDown.cs
+++ KeMBody/CustomControls/NumericUpDown.cs
@@ -1,8 +1,16 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace KeMBody.UI.CustomControls
 {
     public class NumericUpDown : Control
     {
+        static NumericUpDown()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NumericUpDown), new
+               FrameworkPropertyMetadata(typeof(NumericUpDown)));
+        }
+
     }
 }
